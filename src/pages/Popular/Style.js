876ced73d243f47483126ddbd596b6326668d62c
vtorvieira <vieira.vitor42@gmail.com>
2022-10-13 19:09:10 -0300

    feat: criado ultimas rotas e associações aplicadas a elas

---
+++ src/pages/Popular/Style.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+export const DivPopular = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  margin-top: 3%;
+  margin-left: 3%;
+  margin-right: 3%;
+  gap: 50px;
+`;
+
+export const DivCard = styled.div`
+  width: 18%;
+  height: 18%;
+  text-align: center;
+  a {
+    img {
+      border-radius: 5px;
+    }
+  }
+  &:hover {
+    box-shadow: 1px 6px 6px 1px rgba(0,0,0,0.1);
+  }
+  @media (max-width: 768px) {
+    width: 100%;
+    height: 100%;
+  }
+`;

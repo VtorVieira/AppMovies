83c37ca45337fe3432d39713bf5f2f7dc63e7511
vtorvieira <vieira.vitor42@gmail.com>
2022-10-13 19:16:51 -0300

    fix: adicionado BrowserRouter, e provider no arquivo index

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import SearchProvider from './context/searchProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <SearchProvider>
+      <App />
+    </SearchProvider>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
